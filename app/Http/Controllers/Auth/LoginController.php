6fced3c168df1f33461674b8ffc5df476e1a819c
hapo-thomnt <thomnt@haposoft.com>
2019-09-10 16:06:29 +0700

    0910 run login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -66,9 +66,7 @@ class LoginController extends Controller
             'email'   => 'required|email',
             'password' => 'required|min:6'
         ]);
-
-        if (Auth::guard('customer')->attempt(['email' => $request->email, 'password' => $request->password], $request->get('remember'))) {
-
+        if ($this->guard('customer')->attempt($this->credentials($request), $request->filled('remember'))) {
             return redirect()->intended('/customer');
         }
         return back()->withInput($request->only('email', 'remember'));
